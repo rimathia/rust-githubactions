440c5bef313fff11be2d03a4bfe3f2ef524ec3cf
rimathia <rimathia@users.noreply.github.com>
2021-12-28 12:13:16 +0100

    insert clap hello world example to get some dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 use rust_githubactions::saturating_add;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
+    let args = Args::parse();
     println!("100 + 200 = {}", saturating_add(100, 200));
+    for _ in 0..args.count {
+        println!("Hello {}", args.name);
+    }
 }
